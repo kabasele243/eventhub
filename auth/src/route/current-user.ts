f7a6213f4a76480026e572e86b0be7868669b0f2
Franck Kabasele <kabasele467@gmail.com>
2021-09-27 19:48:14 -0400

    restructure auth route

---
+++ auth/src/route/current-user.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('api/users/currentuser', (req, res) => {
+    res.send('My Boy Look At Me');
+})
+
+export { router as currentUserRouter };
