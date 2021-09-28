62789b1a5a552ca8292be87aea267b9d347e1e8e
Franck Kabasele <kabasele467@gmail.com>
2021-09-28 16:12:48 -0400

    add unhandled route error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
-import { currentUserRouter } from "./route/current-user";
-import { signinRouter } from "./route/signin";
-import { signoutRouter } from "./route/signout";
-import { signupRouter } from "./route/signup";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -14,8 +16,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000 My Boy!!!");
+  console.log("Listening on port 3000!!!!!!!!");
 });
