1f0f15ba9221915c50f2173d428719d120fdc2aa
Franck Kabasele <kabasele467@gmail.com>
2021-09-30 15:40:17 -0400

    add more test for user collection

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,53 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'fakeEmail',
+        password: 'password'
+      })
+      .expect(400);
+});
+
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'fakeEmail',
+        password: 'p'
+      })
+      .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+    return request(app)
+      .post('/api/users/signup')
+      .send({
+        email: '',
+        password: ''
+      })
+      .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({ 
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({ 
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400);
+})
