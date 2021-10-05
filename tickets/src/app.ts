2f3f01f32f3ca7caa43ece73dbd54cbd724bc0be
Franck Kabasele <kabasele467@gmail.com>
2021-10-05 16:20:00 -0400

    added more route for tickets and testing them

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,11 +3,10 @@ import 'express-async-errors';
 import { json } from "body-parser";
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from "@eventhubfk/common";
-import { createTicketRouter } from "./routes/new";
-
-
-
-
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,10 +17,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-
 app.use(currentUser);
 
-app.use(createTicketRouter)
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
