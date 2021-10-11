e81a6b447c67b0ebdd8ff00614a829a8c3f6b708
Franck Kabasele <kabasele467@gmail.com>
2021-10-11 15:10:56 -0400

    create order mongoose model

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,17 @@
-import express, { Request, Response }from 'express'
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@eventhubfk/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
+router.get('/api/orders', requireAuth , [
+    body('ticketId')
+        .not()
+        .isEmpty()
+        .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+        .withMessage('TicketId Must Be proived')
+], validateRequest ,async (req: Request, res: Response) => {
     res.send({})
 })
 
