69ee269cb6adba1483f8369792a48032b12e17b1
Franck Kabasele <kabasele467@gmail.com>
2021-10-11 18:25:52 -0400

    fixing https bug

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,6 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ data }) => {
-  // console.log('Landing Bro', data)
   let currentUser = data.currentUser;
   return (
     currentUser ? (
@@ -15,9 +14,7 @@ const LandingPage = ({ data }) => {
 export async function getServerSideProps(context) {
 
   const client = buildClient(context);
-  // console.log(client)
   const { data } = await client.get('/api/users/currentuser');
-  // console.log(data)
 
   return { props : { data }}
 }
--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@eventhubfk/common';
-import {TicketDoc }from './ticket'
+import {TicketDoc }from './ticket';
+
+export { OrderStatus };
 
 interface OrderAttrs {
   userId: string;
