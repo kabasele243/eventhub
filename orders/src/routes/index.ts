81fc62c85478d7e0ae9134195aa93bc55f3de12a
Franck Kabasele <kabasele467@gmail.com>
2021-10-11 19:55:18 -0400

    wrapping up new order test

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
-import express, { Request, Response }from 'express'
+import express, { Request, Response }from 'express';
+import { requireAuth } from '@eventhubfk/common';
+import { Order } from '../models/order'
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-    res.send({})
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+    const order = await Order.find({
+        userId: req.currentUser!.id
+    }).populate('ticket');
+
+    res.send(order);
 })
 
 export { router as indexOrderRouter};
