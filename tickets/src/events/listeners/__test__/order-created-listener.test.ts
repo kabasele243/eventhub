f43777ce8569cc54cd300febdc4fd72d144bed36
Franck Kabasele <kabasele467@gmail.com>
2021-10-14 15:51:46 -0400

    add test for publish event

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -54,3 +54,19 @@ it('acks the message', async () => {
 
   expect(msg.ack).toHaveBeenCalled();
 });
+
+it('publishes a ticket updated event', async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const ticketUpdatedData = await JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+    console.log({ data })
+    console.log({ ticket })
+  expect(data.id).toEqual(ticketUpdatedData.orderId);
+  // expect(data.id).toEqual(ticket.orderId);
+});
